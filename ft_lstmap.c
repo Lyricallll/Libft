1e325f3c5d2bcaed2a2bbe833930eab667dfa3d3
lyricall <lachariotte30@gmail.com>
2024-11-12 23:39:22 +0100

    lstmap patch

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 12:10:16 by agraille          #+#    #+#             */
-/*   Updated: 2024/11/12 15:33:44 by agraille         ###   ########.fr       */
+/*   Updated: 2024/11/12 23:37:17 by agraille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	void	*content;
 
+	if (!lst || !f || !del)
+		return (NULL);
 	new_list = NULL;
-	while (lst != NULL)
+	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		new_node = ft_lstnew(content);
 		if (!new_node)
 		{
 			ft_lstclear(&new_list, del);
+			if (content)
+				del(content);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_node);
